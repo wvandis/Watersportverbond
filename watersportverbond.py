c2d2fff23e91d5d6cb810172e9dd6e006c5d5e7e
wvandis <56789733+wvandis@users.noreply.github.com>
2021-03-23 21:07:15 +0100

    first revision of a working scraper

--- watersportverbond.py
+++ watersportverbond.py
@@ -1,23 +1,70 @@
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
-import re #regex
-import json
-import urllib3
-import pandas as pd 
-import numpy as np 
-from tqdm import tqdm
-import requests
+from selenium import webdriver #3.141.0
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
+import os
+import pandas as pd
+import numpy as np
+import re
+import time
 
-tqdm.pandas()
+# settings 
+url_wedstrijdzeilen='https://www.watersportverbond.nl/kalender/wedstrijden/zeilen'
+path = os.path.dirname(os.path.abspath(__file__))
 
-# First, find the race and classes
-url = 'https://www.watersportverbond.nl/kalender/wedstrijden/zeilen' 
+# Chrome will be used
+options = webdriver.ChromeOptions()
+options.add_argument('ignore-certificate-errors')
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--incognito')
+# setting up selenium driver
+driver = webdriver.Chrome(options=options, executable_path=os.path.join(path,'chromedriver'))
 
-# setting https parser
-content = requests.get(url)
+# opening the website	
+driver.get(url_wedstrijdzeilen)
 
-# beautifulsoup extracting the tbody part
-soup = BeautifulSoup(content.content, "html.parser")
+def by_class(class_name, column_name):
+    content = driver.find_elements_by_class_name(class_name)
+    text = [x.text for x in content]
+    df = pd.DataFrame(text, columns=[column_name])
+    
+    return df
 
-print (soup)
+df_complete=[]
+x=1
+while x==1:
+    # catching event name and club
+    df = by_class('item','Content')
+    df=df['Content'].str.split('\n',n=2, expand=True)
+    df.columns=['Datum','Event','Club']
+    # regex to remove tekst string
+    df['Club'].replace(r'Aan je agenda.*[\n].*|[\n]Aan .*[\n].*','',regex=True, inplace=True)
+    df['Club'].replace(r'[\n]',', ',regex=True, inplace=True)
+
+    if len(df_complete)==0:
+        df_complete=df
+    else:
+        df_complete=df_complete.append(df)
+
+    # print out all the titles.
+    print(df_complete)
+
+    try:
+        # getting the next button by class name
+        button=driver.find_element_by_xpath("""/html/body[@class='template-calendaritemoverviewpage']/div[@id='scrollto-content']/div[@id='initiative-search']/div[@class='g-size-12 g-size-lg-8 faceted-search__results']/div[@class='pagination__container']/ul[@class='pagination']/li[@class='pagination__item pagination__item--next']/a""")
+        # clicking the button
+        driver.execute_script("arguments[0].click();", button)
+    except:
+        x=0
+
+    time.sleep(5)
+
+df_complete.to_excel('watersportverbond.xlsx',sheet_name='zeilen')
+
+# closing connection
+driver.close()
+
